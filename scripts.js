b29299df5387b93edaf2c2284085d5e9f385a108
TarekSammoud <tarek.sammoud@esprit.tn>
2022-06-28 11:22:22 +0200

    Added working slider, rainbow black and clear buttons

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+let color = 'rainbow' ;
+
 function removeallchildnodes(parent)
 {
     while (parent.firstChild) {
@@ -14,7 +16,7 @@ function setBoxSize(boxNB)
     
 
 
-    var boxlength = Math.sqrt((contHeight*contWidth  / boxNB ))   ; 
+    var boxlength = Math.sqrt((contHeight*contWidth  / boxNB )) -3   ; 
 
 
     const squares= document.querySelectorAll('.box')
@@ -36,6 +38,7 @@ function createGrid(boxNB)
     const main=document.querySelector('.container') ; 
     removeallchildnodes(main)
 
+
     for (let i =0 ;i<boxNB ; i++)
     {
         const newT = document.createElement('div') ; 
@@ -48,14 +51,38 @@ function changeColor()
 {
     const squares= document.querySelectorAll('.box')
 
+
+    const rainbowBtn = document.getElementById('rainbowBtn') ;
+    const blackBtn = document.getElementById('blackBtn');
+    const clearBtn = document.getElementById('clearBtn') ; 
+
+    clearBtn.addEventListener('click',()=>{
+        squares.forEach(square =>{
+            square.style.backgroundColor='#cccccc'
+        })
+    })
+
+    blackBtn.addEventListener('click',()=>{
+        color = 'black' ; 
+    }) ;
+     
+    
+    rainbowBtn.addEventListener('click',()=>{
+        color = 'rainbow' ; 
+    }) ;
+     
+
     squares.forEach(square => { 
         square.addEventListener('mouseover', function(e)  {
+            const hex = '#'+(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
             console.log(e) ;
+            if (color=='black')
             square.style.backgroundColor = 'black' ; 
+            if (color=='rainbow')
+            square.style.backgroundColor = hex ; 
     
         }) ;
     } ) ;
 
 
 }
-
